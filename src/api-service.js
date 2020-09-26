bb7b00e7cb6ea41f930ee3133d18b133f1e2fe0e
Rahul Ranjan <rahul.ranjan6344@gmail.com>
2020-09-26 09:55:34 +0530

    popup size adjusted

--- src/api-service.js
+++ src/api-service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const base="https://elixar.herokuapp.com/"
-const base=" http://127.0.0.1:8000/"
+const base="https://elixar.herokuapp.com/"
+// const base=" http://127.0.0.1:8000/"
 const apikey = '8fdef991-fced-11ea-9fa5-0200cd936042'
 const otpBaseUrl = 'https://2factor.in/API/V1/'
 // const sms = 'SMS'
